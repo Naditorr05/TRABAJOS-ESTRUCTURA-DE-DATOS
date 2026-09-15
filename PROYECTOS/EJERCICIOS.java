ea3ece4704c429ad251e2497cb219e63b3b4e622
Naditorr05 <narenjosue.dimatetorres@outlook.com>
2026-09-14 21:05:41 -0500

    Fix forEach method call on inventario2 array

--- PROYECTOS/EJERCICIOS.java
+++ PROYECTOS/EJERCICIOS.java
@@ -9,4 +9,4 @@ for(int i=0 ; i<inventario.size ; i++){
 }
 
 String [] inventario2 = {"Mango", "Pera", "Fresa", "Uva"};
-inventario2.for.each(System.out::println);
+inventario2.forEach(System.out::println);
