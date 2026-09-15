0060b01970e9d3e4b9ae3fcd667b3fc7b7e9acef
Naditorr05 <narenjosue.dimatetorres@outlook.com>
2026-09-14 21:03:35 -0500

    Create EJERCICIOS.java

---
+++ PROYECTOS/EJERCICIOS.java
@@ -0,0 +1,12 @@
+System.out.println("INVENTARIO CON BUCLE FOR CORTO");
+for(String producto:inventario){
+    System.out.println("-" + producto);
+  }
+
+System.out.println("\n INVENTARIO ");
+for(int i=0 ; i<inventario.size ; i++){
+  System.out.println(inventario.get(i));
+}
+
+String [] inventario2 = {"Mango", "Pera", "Fresa", "Uva"};
+for.each.i()
